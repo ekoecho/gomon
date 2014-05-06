b11e87b26fbc4836221979597955ae8f1ecb40a4
Graham Clark <graham.clark@ekoecho.com>
2014-05-06 22:28:54 +0100

    added some memory information and a .gitignore file

--- gomon.go
+++ gomon.go
@@ -12,6 +12,11 @@ type Loadavg struct {
 	one_min, five_min, ten_min string
 }
 
+type MemInfo struct {
+	totalMem string
+	freeMem  string
+}
+
 func getLoadAvg() Loadavg {
 	load, err := ioutil.ReadFile("/proc/loadavg")
 	if err != nil {
@@ -27,9 +32,26 @@ func getLoadAvg() Loadavg {
 
 }
 
+func getMemInfo() MemInfo {
+
+	mem, err := ioutil.ReadFile("/proc/meminfo")
+	if err != nil {
+		panic(err)
+	}
+
+	m := MemInfo{}
+	m.totalMem = strings.Split(string(mem), "\n")[0]
+	m.freeMem = strings.Split(string(mem), "\n")[1]
+
+	return m
+}
+
 func main() {
 	load := getLoadAvg()
+	mem := getMemInfo()
 
 	fmt.Println(load.one_min, load.five_min, load.ten_min)
+	fmt.Println(mem.totalMem)
+	fmt.Println(mem.freeMem)
 
 }
